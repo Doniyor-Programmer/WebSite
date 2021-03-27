0a927e251f2c9d593d2f5e98b291692510826b04
Doniyor-Programmer <74669425+Doniyor-Programmer@users.noreply.github.com>
2021-03-27 20:34:57 +0500

    <a href="uglyExample/uglyExample.h

--- Call/js/script.js
+++
@@ -1,9 +0,0 @@
-let numberText = document.querySelector(".numberText"),
-    numberBtn = document.querySelector(".numberBtn"),
-    numberLink = document.querySelector(".numberLink");
-
-numberBtn.addEventListener("click", () => {
-    numberLink.href = `tel:${numberText.value}`;
-    numberLink.textContent = `Action with this number ${numberText.value}`;
-    numberText.value = ``;
-});
