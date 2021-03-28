b1f29d4102bbe3085f872561fd4da5fbe38de61d
Doniyor-Programmer <74669425+Doniyor-Programmer@users.noreply.github.com>
2021-03-28 11:24:56 +0500

    test

---
+++ test/js/script.js
@@ -0,0 +1,250 @@
+let true1 = document.querySelector(".true1"),
+    true2 = document.querySelector(".true2"),
+    true3 = document.querySelector(".true3"),
+    true4 = document.querySelector(".true4"),
+    true5 = document.querySelector(".true5"),
+    error1 = document.querySelector(".error1"),
+    error2 = document.querySelector(".error2"),
+    error3 = document.querySelector(".error3"),
+    error4 = document.querySelector(".error4"),
+    error5 = document.querySelector(".error5"),
+    error6 = document.querySelector(".error6"),
+    error7 = document.querySelector(".error7"),
+    error8 = document.querySelector(".error8"),
+    error9 = document.querySelector(".error9"),
+    error10 = document.querySelector(".error10"),
+    error11 = document.querySelector(".error11"),
+    error12 = document.querySelector(".error12"),
+    error13 = document.querySelector(".error13"),
+    error14 = document.querySelector(".error14"),
+    error15 = document.querySelector(".error15"),
+    n1 = document.querySelector(".n1"),
+    n2 = document.querySelector(".n2"),
+    n3 = document.querySelector(".n3"),
+    n4 = document.querySelector(".n4"),
+    n5 = document.querySelector(".n5"),
+    n6 = document.querySelector(".n6"),
+    n7 = document.querySelector(".n7"),
+    n8 = document.querySelector(".n8"),
+    n9 = document.querySelector(".n9"),
+    n10 = document.querySelector(".n10"),
+    n11 = document.querySelector(".n11"),
+    n12 = document.querySelector(".n12"),
+    n13 = document.querySelector(".n13"),
+    n14 = document.querySelector(".n14"),
+    n15 = document.querySelector(".n15"),
+    n16 = document.querySelector(".n16"),
+    n17 = document.querySelector(".n17"),
+    n18 = document.querySelector(".n18"),
+    n19 = document.querySelector(".n19"),
+    n20 = document.querySelector(".n20");
+
+n1.addEventListener("change", () => {
+    true1.style.cssText = "background-color: green";
+    n2.style.display = "none";
+    error1.style.display = "none";
+    n3.style.display = "none";
+    error2.style.display = "none";
+    n4.style.display = "none";
+    error3.style.display = "none";
+});
+
+n5.addEventListener("change", () => {
+    true2.style.cssText = "background-color: green";
+    n6.style.display = "none";
+    error4.style.display = "none";
+    n7.style.display = "none";
+    error5.style.display = "none";
+    n8.style.display = "none";
+    error6.style.display = "none";
+});
+
+n9.addEventListener("change", () => {
+    true3.style.cssText = "background-color: green";
+    n10.style.display = "none";
+    error7.style.display = "none";
+    n11.style.display = "none";
+    error8.style.display = "none";
+    n12.style.display = "none";
+    error9.style.display = "none";
+});
+
+n13.addEventListener("change", () => {
+    true4.style.cssText = "background-color: green";
+    n14.style.display = "none";
+    error10.style.display = "none";
+    n15.style.display = "none";
+    error11.style.display = "none";
+    n16.style.display = "none";
+    error12.style.display = "none";
+});
+
+n17.addEventListener("change", () => {
+    true5.style.cssText = "background-color: green";
+    n18.style.display = "none";
+    error13.style.display = "none";
+    n19.style.display = "none";
+    error14.style.display = "none";
+    n20.style.display = "none";
+    error15.style.display = "none";
+});
+
+
+
+n2.addEventListener("change", () => {
+    error1.style.cssText = "background-color: red";
+    n1.style.display = "none";
+    true1.style.display = "none";
+    n3.style.display = "none";
+    error2.style.display = "none";
+    n4.style.display = "none";
+    error3.style.display = "none";
+});
+
+n3.addEventListener("change", () => {
+    error2.style.cssText = "background-color: red";
+    n1.style.display = "none";
+    true1.style.display = "none";
+    n2.style.display = "none";
+    error1.style.display = "none";
+    n4.style.display = "none";
+    error3.style.display = "none";
+});
+
+n4.addEventListener("change", () => {
+    error3.style.cssText = "background-color: red";
+    n1.style.display = "none";
+    true1.style.display = "none";
+    n2.style.display = "none";
+    error1.style.display = "none";
+    n3.style.display = "none";
+    error2.style.display = "none";
+});
+
+
+
+n6.addEventListener("change", () => {
+    error4.style.cssText = "background-color: red";
+    n5.style.display = "none";
+    true2.style.display = "none";
+    n7.style.display = "none";
+    error5.style.display = "none";
+    n8.style.display = "none";
+    error6.style.display = "none";
+});
+
+n7.addEventListener("change", () => {
+    error5.style.cssText = "background-color: red";
+    n5.style.display = "none";
+    true2.style.display = "none";
+    n6.style.display = "none";
+    error4.style.display = "none";
+    n8.style.display = "none";
+    error6.style.display = "none";
+});
+
+n8.addEventListener("change", () => {
+    error6.style.cssText = "background-color: red";
+    n5.style.display = "none";
+    true2.style.display = "none";
+    n6.style.display = "none";
+    error4.style.display = "none";
+    n7.style.display = "none";
+    error5.style.display = "none";
+});
+
+
+
+n10.addEventListener("change", () => {
+    error7.style.cssText = "background-color: red";
+    n9.style.display = "none";
+    true3.style.display = "none";
+    n11.style.display = "none";
+    error8.style.display = "none";
+    n12.style.display = "none";
+    error9.style.display = "none";
+});
+
+n11.addEventListener("change", () => {
+    error8.style.cssText = "background-color: red";
+    n9.style.display = "none";
+    true3.style.display = "none";
+    n10.style.display = "none";
+    error7.style.display = "none";
+    n12.style.display = "none";
+    error9.style.display = "none";
+});
+
+n12.addEventListener("change", () => {
+    error9.style.cssText = "background-color: red";
+    n9.style.display = "none";
+    true3.style.display = "none";
+    n10.style.display = "none";
+    error7.style.display = "none";
+    n11.style.display = "none";
+    error8.style.display = "none";
+});
+
+
+
+n14.addEventListener("change", () => {
+    error10.style.cssText = "background-color: red";
+    n13.style.display = "none";
+    true4.style.display = "none";
+    n15.style.display = "none";
+    error11.style.display = "none";
+    n16.style.display = "none";
+    error12.style.display = "none";
+});
+
+n15.addEventListener("change", () => {
+    error11.style.cssText = "background-color: red";
+    n13.style.display = "none";
+    true4.style.display = "none";
+    n14.style.display = "none";
+    error10.style.display = "none";
+    n16.style.display = "none";
+    error12.style.display = "none";
+});
+
+n16.addEventListener("change", () => {
+    error12.style.cssText = "background-color: red";
+    n13.style.display = "none";
+    true4.style.display = "none";
+    n14.style.display = "none";
+    error10.style.display = "none";
+    n15.style.display = "none";
+    error11.style.display = "none";
+});
+
+
+
+n18.addEventListener("change", () => {
+    error13.style.cssText = "background-color: red";
+    n17.style.display = "none";
+    true5.style.display = "none";
+    n19.style.display = "none";
+    error14.style.display = "none";
+    n20.style.display = "none";
+    error15.style.display = "none";
+});
+
+n19.addEventListener("change", () => {
+    error14.style.cssText = "background-color: red";
+    n17.style.display = "none";
+    true5.style.display = "none";
+    n18.style.display = "none";
+    error13.style.display = "none";
+    n20.style.display = "none";
+    error15.style.display = "none";
+});
+
+n20.addEventListener("change", () => {
+    error15.style.cssText = "background-color: red";
+    n17.style.display = "none";
+    true5.style.display = "none";
+    n18.style.display = "none";
+    error13.style.display = "none";
+    n19.style.display = "none";
+    error14.style.display = "none";
+});
